956270c6d96647c14f135a47e221fb65654bd246
miloram <miloram04@gmail.com>
2024-08-26 21:29:05 -0700

    Lesson 53

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,7 @@ from app import views
 
 urlpatterns = [
     path('', views.job_list, name='jobs_home'),
+    path('hello/', views.hello, name='hello'),
     path('job/<int:id>', views.job_detail, name='jobs_detail'),
     # path('job/<str:id>', views.job_detail),
 ]
--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import redirect
 from django.urls import reverse
+from django.template import loader
 
 job_title = [
     "First job",
@@ -15,18 +16,35 @@ job_description = [
     "Third Job Description"
 ]
 
+
+
 # Create your views here.
 #def hello(request):
 #    return HttpResponse("<h3>Hello World</h3>")
 
+class TempClass:
+    x = 5
+
+
+def hello(request):
+    list = ["alpha", "beta"]
+    temp = TempClass()
+    is_authenticated = False
+    context={"name":"Django", "age":0, "first_list":list, "temp_object":TempClass,
+             "is_authenticated":is_authenticated}
+    # return HttpResponse(template.render(context, request))
+    return render(request, "app/hello.html",context)
+
 def job_list(request):
-    list_of_jobs = "<ul>"
-    for j in job_title:
-        job_id = job_title.index(j)
-        detail_url = reverse('jobs_detail', args=(job_id,))
-        list_of_jobs += f"<li><a href='{detail_url}'>{j}</li>"
-    list_of_jobs += "</ul>"
-    return HttpResponse(list_of_jobs)
+    # list_of_jobs = "<ul>"
+    # for j in job_title:
+    #     job_id = job_title.index(j)
+    #     detail_url = reverse('jobs_detail', args=(job_id,))
+    #     list_of_jobs += f"<li><a href='{detail_url}'>{j}</li>"
+    # list_of_jobs += "</ul>"
+    # return HttpResponse(list_of_jobs)
+    context={"job_title_list":job_title}
+    return render(request, "app/index.html", context)
 
 def job_detail(request, id):
     print(type(id))
@@ -34,8 +52,10 @@ def job_detail(request, id):
     try:
         if id ==0:
             return redirect(reverse('jobs_home'))
-        return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
-        return HttpResponse(return_html)
+        # return_html = f"<h1>{job_title[id]}</h1> <h3>{job_description[id]}</h3>"
+        # return HttpResponse(return_html)
+        context={"job_title":job_title[id], "job_description":job_description[id]}
+        return render(request, "app/job_detail.html", context)
     except:
         return HttpResponseNotFound("Not Found")
 
